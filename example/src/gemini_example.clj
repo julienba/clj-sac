88beabc368aacde5e9f5fc3133b5f41f11260f43
julien bille <julien.bille@gmail.com>
2025-09-15 12:02:36 +0200

    Add gemini support (#2)
    * Add gemini support
    
    * Update dependencies
    
    * Add documentation

---
+++ example/src/gemini_example.clj
@@ -0,0 +1,18 @@
+(ns gemini-example
+  (:require [clj-sac.llm.http.gemini :as gemini]))
+
+(def TOKEN (System/getenv "GEMINI_API_KEY"))
+
+(comment
+  (def answer (gemini/chat-completion
+               {:messages [{:content "Write me a small haiku about Amsterdam"
+                            :role "user"}]
+                :model "gemini-2.5-flash"}
+               {:headers {"x-goog-api-key" TOKEN}}))
+  (get-in answer [:body :candidates 0 :content :parts 0 :text])
+  ; "Canals gently flow,\nBicycles glide on the bridges,\nDutch charm fills the air."
+  )
+
+
+
+
